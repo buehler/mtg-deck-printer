4190b2ae1d29ee067222983a859575b9f6d6d8d3
Christoph Bühler <christoph@smartive.ch>
2020-08-30 11:29:38 +0200

    remove purge

--- webpack.config.js
+++ webpack.config.js
@@ -67,20 +67,6 @@ module.exports = (_, { mode }) => {
         ],
       })
     );
-    config.plugins.push(
-      new PurgecssPlugin({
-        paths: glob.sync('./src/**/*', { nodir: true }),
-        defaultExtractor: (content) => {
-          // Capture as liberally as possible, including things like `h-(screen-1.5)`
-          const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-
-          // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-          const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-
-          return broadMatches.concat(innerMatches);
-        },
-      })
-    );
   } else {
     config.plugins.push(
       new HtmlWebpackPlugin({
